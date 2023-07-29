7f7eb4a21685b80490f4cf8b36cf433e9cc57f90
Jean-Paul Paoli <contact@paolim.fr>
2023-07-29 17:14:21 +0200

    aider: The Pydantic model `Query` is now used to handle form data in the `index` function in `latest/app.py`.
    # Aider chat conversation:
    
    USER: I added these *read-write* files: latest/templates/index.html
    
    ASSISTANT: Great, now that we have the `latest/templates/index.html` file in read-write mode, we can proceed with the changes.
    
    FastAPI uses Pydantic models to handle request data. In your case, you're expecting a POST request with a `query` parameter. We'll need to create a Pydantic model to handle this.
    
    Also, FastAPI's `Request` object doesn't have a `form` attribute like Flask's `request` object. Instead, we'll use the Pydantic model to get the form data.
    
    Here's how you can modify the `latest/app.py` file:
    
    ```python
    latest/app.py
    <<<<<<< ORIGINAL
    from fastapi import FastAPI, Request
    from fastapi.templating import Jinja2Templates
    from myaideas import main
    from pydantic import BaseModel
    
    app = FastAPI()
    
    templates = Jinja2Templates(directory="templates")
    
    @app.post("/")
    def index(request: Request, query: str):
        results = main(query)
        return templates.TemplateResponse('index.html', {"request": request, "results": results})
    
    @app.get("/")
    def read_root(request: Request):
        return templates.TemplateResponse('index.html', {"request": request})
    =======
    from fastapi import FastAPI, Request
    from fastapi.templating import Jinja2Templates
    from myaideas import main
    from pydantic import BaseModel
    
    class Query(BaseModel):
        query: str
    
    app = FastAPI()
    
    templates = Jinja2Templates(directory="templates")
    
    @app.post("/")
    def index(query: Query, request: Request):
        results = main(query.query)
        return templates.TemplateResponse('index.html', {"request": request, "results": results})
    
    @app.get("/")
    def read_root(request: Request):
        return templates.TemplateResponse('index.html', {"request": request})
    >>>>>>> UPDATED
    ```
    
    In the updated code, we're using a Pydantic model named `Query` to handle the form data. We're also passing the `Query` model as a parameter to the `index` function, and we're using `query.query` to get the query string.
    
    After making this change, you should be able to run your FastAPI application with Uvicorn and handle POST requests with a `query` parameter. If you're still encountering issues, please let me know.

--- latest/app.py
+++ latest/app.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from myaideas import main
+from pydantic import BaseModel
+
+class Query(BaseModel):
+    query: str
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
 @app.post("/")
-def index(request: Request, query: str):
-    results = main(query)
+def index(query: Query, request: Request):
+    results = main(query.query)
     return templates.TemplateResponse('index.html', {"request": request, "results": results})
 
 @app.get("/")
