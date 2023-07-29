752c66073b5d1dd6be6037375319badfa07faf50
Jean-Paul Paoli <contact@paolim.fr>
2023-07-29 17:08:25 +0200

    remove utils

--- latest/utils.py
+++ latest/utils.py
@@ -25,30 +25,3 @@ def read_str_prompt(filepath: str):
     return template
 
 #************* - A casser dans class MarkdownReader
-
-def correct_markdown(self, content: str) -> str:
-    """
-    Corrects headers inside markdown links.
-    """
-    pattern = r"\[\s*\n*(#{1,6}.*?)\n*\]\((.*?)\)"
-    def replacer(match):
-        header = match.group(1)
-        link = match.group(2)
-        return header + "\n[Link](" + link + ")"
-    content = re.sub(pattern, replacer, content)
-    return content
-
-
-def parse_tups(self, filepath: Path, errors: str = "ignore") -> List[Tuple[Optional[str], str]]:
-    """
-    Parses a file into tuples.
-    """
-    with open(filepath, "r", encoding="utf-8") as f:
-        content = f.read()
-    if self._remove_hyperlinks:
-        content = self.remove_hyperlinks(content)
-    if self._remove_images:
-        content = self.remove_images(content)
-    content = self.correct_markdown(content)
-    markdown_tups = self.markdown_to_tups(content)
-    return markdown_tups
